30e2210ca45392755dcc313da92b4688c1f26917
JohnRudolph1 <johnrudolph963@gmail.com>
2021-12-16 08:28:47 -0600

    updated html

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const { response } = require("express");
 const express = require("express");
 const { Server } = require("http");
 const app = express();
-const url = "https://www.theguardian.com/us";
+const url = "https://www.twitter.com";
 axios(url).then((response) => {
   const html = response.data;
   const $ = cheerio.load(html);
   const comments = [];
-  $(".fc-item__title", html).each(function () {
+  $("span", html).each(function () {
     const title = $(this).text();
     const url = $(this).find("a").attr("href");
+    const date = $(this).find("a").attr("href");
+    const picture = $(this).find("a").attr("href");
     comments.push({
+      picture,
       title,
+      date,
       url,
     });
   });
